e94be322900118c73fe924b2efec5617ea31dd6b
Bruno Nascimento Marques <mv.brunonmarques@gmail.com>
2022-01-11 10:34:47 -0300

    Initial Commit

---
+++ conversorAPP/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  conversorAPP
+//
+//  Created by Bruno Nascimento Marques on 10/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    @IBOutlet weak var lbUnit: UILabel!
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+        switch lbUnit.text! {
+        case "Temperatura":
+            lbUnit.text = "Peso"
+            btUnit1.setTitle("Kilograma", for: .normal)
+            btUnit2.setTitle("Libra", for: .normal)
+        case "Peso":
+            lbUnit.text = "Moeda"
+            btUnit1.setTitle("Real", for: .normal)
+            btUnit2.setTitle("Dólar", for: .normal)
+        case "Moeda":
+            lbUnit.text = "Distância"
+            btUnit1.setTitle("Metro", for: .normal)
+            btUnit2.setTitle("Kilômetro", for: .normal)
+        default:
+            lbUnit.text = "Temperatura"
+            btUnit1.setTitle("Celsius", for: .normal)
+            btUnit2.setTitle("Farenheint", for: .normal)
+        }
+        convert(nil)
+    }
+    
+    @IBAction func convert(_ sender: UIButton?) {
+        if let sender = sender {
+            if sender == btUnit1 {
+                btUnit2.alpha = 0.5
+            }else {
+                btUnit1.alpha = 0.5
+            }
+            sender.alpha = 1.0
+        }
+        
+        switch lbUnit.text! {
+            case "Temperatura":
+                calcTemperatura()
+            case "Peso":
+                calcPeso()
+            case "Moeda":
+                calcMoeda()
+            default:
+                calcDistancia()
+            }
+            view.endEditing(true)
+            let result = Double(lbResult.text!)!
+            lbResult.text = String(format: "%.2f", result)
+        
+    }
+    
+    func calcTemperatura() {
+        guard let temperatura = Double(tfValue.text!) else {return}
+            if btUnit1.alpha == 1.0 {
+                lbResultUnit.text = "Farenheint"
+                lbResult.text = String(temperatura * 1.8 + 32.0)
+            }else {
+                lbResultUnit.text = "Celsius"
+                lbResult.text = String((temperatura - 32.0) / 1.8)
+            }
+    }
+    func calcPeso() {
+        guard let peso = Double(tfValue.text!) else {return}
+            if btUnit1.alpha == 1.0 {
+                lbResultUnit.text = "Libra"
+                lbResult.text = String(peso * 2.205)
+            }else {
+                lbResultUnit.text = "Kilograma"
+                lbResult.text = String(peso / 2.205)
+            }
+    }
+    func calcMoeda() {
+        guard let moeda = Double(tfValue.text!) else {return}
+            if btUnit1.alpha == 1.0 {
+                lbResultUnit.text = "Dólar"
+                lbResult.text = String(moeda / 5.66)
+            }else {
+                lbResultUnit.text = "Real"
+                lbResult.text = String(moeda * 5.66)
+            }
+    }
+    func calcDistancia() {
+        guard let distancia = Double(tfValue.text!) else {return}
+            if btUnit1.alpha == 1.0 {
+                lbResultUnit.text = "Kilômetro"
+                lbResult.text = String(distancia / 1000)
+            }else {
+                lbResultUnit.text = "Metro"
+                lbResult.text = String(distancia * 1000)
+            }
+    }
+    
+}
+
